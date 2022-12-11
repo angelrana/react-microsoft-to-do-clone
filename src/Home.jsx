ca2cc2ae326fb2f88c29e039fc1f3d9e2de2a98f
Angel Thapa <ennjelo@gmail.com>
2022-12-11 13:45:03 +0530

    Working on the sidebar

---
+++ src/Home.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import MainLayout from './MainLayout'
+import Sidebar from './Sidebar'
+
+const Home = () => {
+  return (
+    <div>
+          <Sidebar />
+    </div>
+  )
+}
+
+export default Home;
---
+++ src/Sidebar.jsx
@@ -0,0 +1,119 @@
+import * as React from "react";
+import Box from "@mui/material/Box";
+import Drawer from "@mui/material/Drawer";
+import CssBaseline from "@mui/material/CssBaseline";
+import AppBar from "@mui/material/AppBar";
+import Toolbar from "@mui/material/Toolbar";
+import List from "@mui/material/List";
+import Typography from "@mui/material/Typography";
+import Divider from "@mui/material/Divider";
+import ListItem from "@mui/material/ListItem";
+import ListItemButton from "@mui/material/ListItemButton";
+import ListItemIcon from "@mui/material/ListItemIcon";
+import ListItemText from "@mui/material/ListItemText";
+import InboxIcon from "@mui/icons-material/MoveToInbox";
+import MailIcon from "@mui/icons-material/Mail";
+import MainLayout from "./MainLayout";
+import LightModeIcon from "@mui/icons-material/LightMode";
+import StarBorderIcon from "@mui/icons-material/StarBorder";
+import HomeIcon from "@mui/icons-material/Home";
+import PersonOutlineIcon from "@mui/icons-material/PersonOutline";
+import GradientIcon from "@mui/icons-material/Gradient";
+import ShoppingCartIcon from "@mui/icons-material/ShoppingCart";
+import HourglassEmptyIcon from "@mui/icons-material/HourglassEmpty";
+
+const drawerWidth = 280;
+
+export default function Sidebar() {
+  return (
+    <Box sx={{ display: "flex" }}>
+      <CssBaseline />
+
+      <Drawer
+        sx={{
+          width: drawerWidth,
+          flexShrink: 0,
+          "& .MuiDrawer-paper": {
+            width: drawerWidth,
+            boxSizing: "border-box",
+          },
+        }}
+        variant="permanent"
+        anchor="left">
+        <Toolbar />
+        <Divider />
+        <List>
+          {/* {["My Day", "Important", "Planned", "Assigned to me", "Tasks"].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 5 === 1 ? <LightModeIcon /> : <StarBorderIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))} */}
+
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <LightModeIcon />
+              </ListItemIcon>
+              <ListItemText primary="My Day" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <StarBorderIcon />
+              </ListItemIcon>
+              <ListItemText primary="Important" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <GradientIcon />
+              </ListItemIcon>
+              <ListItemText primary="Planned" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <PersonOutlineIcon />
+              </ListItemIcon>
+              <ListItemText primary="Assigned to me" />
+            </ListItemButton>
+          </ListItem>
+          <ListItem disablePadding>
+            <ListItemButton>
+              <ListItemIcon>
+                <HomeIcon />
+              </ListItemIcon>
+              <ListItemText primary="Tasks" />
+            </ListItemButton>
+          </ListItem>
+        </List>
+        <Divider />
+        <List>
+          {["Getting Started", "Groceries"].map((text, index) => (
+            <ListItem key={text} disablePadding>
+              <ListItemButton>
+                <ListItemIcon>
+                  {index % 2 === 0 ? <HourglassEmptyIcon /> : <ShoppingCartIcon />}
+                </ListItemIcon>
+                <ListItemText primary={text} />
+              </ListItemButton>
+            </ListItem>
+          ))}
+        </List>
+      </Drawer>
+      <Box
+        component="main"
+        sx={{ flexGrow: 1, bgcolor: "background.default", p: 3 }}>
+        <MainLayout />
+      </Box>
+    </Box>
+  ); 
+}
