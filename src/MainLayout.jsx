4a51c85466f0c25b72718f03121cf06368833ac0
Angel Thapa <ennjelo@gmail.com>
2022-12-12 22:30:32 +0530

    working on the state of the notes

--- src/MainLayout.jsx
+++ src/MainLayout.jsx
@@ -1,28 +1,95 @@
-import React from 'react'
-import { Box } from '@mui/material';
+import React, { useState, useEffect } from 'react'
+import { Box, TextField } from '@mui/material';
 import Toolbar from '@mui/material/Toolbar';
 import Typography from '@mui/material/Typography';
+import InputAdornment from "@mui/material/InputAdornment";
+import PanoramaFishEyeIcon from "@mui/icons-material/PanoramaFishEye";
+import produce from 'immer';
+
+const Notes = (props) =>
+  props.data.map((note) => (
+    <Box
+      component="main"
+      sx={{
+        flexGrow: 1,
+        bgcolor: "pink",
+        p: 3,
+        width: 128,
+        height: 100,
+      }}>
+      <Typography
+        fullWidth
+        size="small"
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <PanoramaFishEyeIcon />
+            </InputAdornment>
+          ),
+        }}>
+        {note.text}
+      </Typography>
+    </Box>
+  ));
+
 const MainLayout = () => {
+    const initialData = [{ text: "Loading Notes" }];
+    const [data, setData] = useState(initialData);
+   
+     const handleKeyDown = (event) => {
+       if (event.key === "Enter") {
+           handleClick();
+       }
+     };
+
+    const handleClick = () => {
+        const text = document.querySelector('#noteinput').value.trim();
+        if (text) {
+            const nextState = produce(data, draftState => {
+                draftState.push({ text })
+            });
+            document.querySelector('#noteinput').value = ''
+
+            if (typeof window !== 'undefined') {
+                localStorage.setItem('data', JSON.stringify(nextState))
+            }
+
+            setData(nextState);
+        }
+    };
+
+     useEffect(() => {
+       if (typeof window !== "undefined") {
+         const getData = localStorage.getItem("data");
+         if (getData !== "" && getData !== "null") {
+           return setData(JSON.parse(getData));
+         }
+         return setData([]);
+       }
+     }, {data});
+
+
+
   return (
     <Box
       component="main"
       sx={{ flexGrow: 1, bgcolor: "background.default", p: 3 }}>
       <Toolbar />
-      <Typography paragraph>
-        Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod
-        tempor incididunt ut labore et dolore magna aliqua. Rhoncus dolor purus
-        non enim praesent elementum facilisis leo vel. Risus at ultrices mi
-        tempus imperdiet. Semper risus in hendrerit gravida rutrum quisque non
-        tellus. Convallis convallis tellus id interdum velit laoreet id donec
-        ultrices. Odio morbi quis commodo odio aenean sed adipiscing. Amet nisl
-        suscipit adipiscing bibendum est ultricies integer quis. Cursus euismod
-        quis viverra nibh cras. Metus vulputate eu scelerisque felis imperdiet
-        proin fermentum leo. Mauris commodo quis imperdiet massa tincidunt. Cras
-        tincidunt lobortis feugiat vivamus at augue. At augue eget arcu dictum
-        varius duis at consectetur lorem. Velit sed ullamcorper morbi tincidunt.
-        Lorem donec massa sapien faucibus et molestie ac.
-      </Typography>
+      <Notes data={data}></Notes>
 
+      <TextField
+        onKeyDown={handleKeyDown}
+        id="noteinput"
+        fullWidth
+        size="small"
+        placeholder='Try typing "paying utility bills"'
+        InputProps={{
+          startAdornment: (
+            <InputAdornment position="start">
+              <PanoramaFishEyeIcon />
+            </InputAdornment>
+          ),
+        }}></TextField>
     </Box>
   );
 }
